ba5fbf6ea830b7718abe9cbdba60caa8570b3ab8
Muhammad Faizan Qureshi <muhammadfaizan9222@gmail.com>
2025-03-08 15:06:32 +0500

    Index.tsx updated

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
-import { StatusBar, SafeAreaView } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
+import { StatusBar, View, Platform } from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Welcome from "../../screens/Welcome";
 import Login from "../../screens/Login";
@@ -23,39 +22,38 @@ import LandlordEditProfile from "../../screens/LandlordEditProfile";
 import VerificationWaiting from "../../screens/VerificationWaiting";
 import PropertyCard from "../../screens/PropertyCard";
 import FullScreenImage from "../../screens/FullScreenImage";
+// import ChangePasswordScreen from "../../screens/ChangePasswordScreen";
 
 const Stack = createNativeStackNavigator();
 
-const App = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Welcome"
-      >
-        <Stack.Screen name="Welcome" component={Welcome} />
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Signup" component={Signup} />
-        <Stack.Screen name="HomeScreen" component={HomeScreen} />
-        <Stack.Screen name="LandlordHome" component={LandlordHome} />
-        <Stack.Screen name="PropertyDetailUser"component={PropertyDetailUser}/>
-        <Stack.Screen name="Notifications" component={Notifications} />
-        <Stack.Screen name="Bookings" component={Bookings} />
-        <Stack.Screen name="ProfilePage" component={ProfilePage} />
-        <Stack.Screen name="ConfirmPay" component={ConfirmPay} />
-        <Stack.Screen name="SuccessPage" component={SuccessPage} />
-        <Stack.Screen name="EditProfile" component={EditProfile} />
-        <Stack.Screen name="PropertyList" component={PropertyList} />
-        <Stack.Screen name="SuccessPagePropertyUpload" component={SuccessPagePropertyUpload}/>
-        <Stack.Screen name="LandlordNotifications" component={LandlordNotifications} />
-        <Stack.Screen name="YourProperties" component={YourProperties} />
-        <Stack.Screen name="LandlordProfile" component={LandlordProfile} />
-        <Stack.Screen name="LandlordEditProfile" component={LandlordEditProfile}/>
-        <Stack.Screen name="VerificationWaiting" component={VerificationWaiting}/>
-        <Stack.Screen name="PropertyCard" component={PropertyCard} />
-        <Stack.Screen name="FullScreenImage" component={FullScreenImage} />
-      </Stack.Navigator>
-    </>
-  );
-};
+const App = () => (
+  <View style={{ flex: 1, marginTop: Platform.OS === "ios" ? -40 : 0 }}>
+    <StatusBar barStyle="dark-content" />
+    <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Welcome">
+      <Stack.Screen name="Welcome" component={Welcome} />
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Signup" component={Signup} />
+      <Stack.Screen name="HomeScreen" component={HomeScreen} />
+      <Stack.Screen name="LandlordHome" component={LandlordHome} />
+      <Stack.Screen name="PropertyDetailUser" component={PropertyDetailUser} />
+      <Stack.Screen name="Notifications" component={Notifications} />
+      <Stack.Screen name="Bookings" component={Bookings} />
+      <Stack.Screen name="ProfilePage" component={ProfilePage} />
+      <Stack.Screen name="ConfirmPay" component={ConfirmPay} />
+      <Stack.Screen name="SuccessPage" component={SuccessPage} />
+      <Stack.Screen name="EditProfile" component={EditProfile} />
+      {/* <Stack.Screen name="ChangePasswordScreen" component={ChangePasswordScreen} /> */}
+      <Stack.Screen name="FullScreenImage" component={FullScreenImage} />
+      <Stack.Screen name="PropertyList" component={PropertyList} />
+      <Stack.Screen name="SuccessPagePropertyUpload" component={SuccessPagePropertyUpload} />
+      <Stack.Screen name="LandlordNotifications" component={LandlordNotifications} />
+      <Stack.Screen name="YourProperties" component={YourProperties} />
+      <Stack.Screen name="LandlordProfile" component={LandlordProfile} />
+      <Stack.Screen name="LandlordEditProfile" component={LandlordEditProfile} />
+      <Stack.Screen name="VerificationWaiting" component={VerificationWaiting} />
+      <Stack.Screen name="PropertyCard" component={PropertyCard} />
+    </Stack.Navigator>
+  </View>
+);
 
 export default App;
